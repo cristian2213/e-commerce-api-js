20352e4a54a61802282bb8d0f84a03cefecb8ddb
cristian2213 <crislm1322@gmail.com>
2022-01-19 05:03:22 -0500

    :sparkles: Established sequelize-cli to migrations, models and seeders

---
+++ db/migrations/20220119085609-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        // NOTE HERE I NEED TO APPLY ALL CONSTRAINS
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      emailVerifiedAt: {
+        type: Sequelize.DATE,
+      },
+      token: {
+        type: Sequelize.STRING,
+      },
+      tokenExpiration: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
--- test/api.spec.ts
+++
@@ -1,9 +0,0 @@
-const request = 'supertest';
-
-// import app from '../src/app';
-
-// describe('GET /api/v1/auth', () => {
-//   it('should return 200 OK', () => {
-//     return request(app).get('/api').expect(200);
-//   });
-// });
